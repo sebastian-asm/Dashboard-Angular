f16f09c7a5bf52a1865046f10c0960e11678bc98
Sebastián <sebtstian@gmail.com>
2020-07-05 19:46:15 -0300

    creacion de componentes base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ClientesComponent } from './components/clientes/clientes.component';
+import { TableroComponent } from './components/tablero/tablero.component';
+import { EditarClienteComponent } from './components/editar-cliente/editar-cliente.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { ConfigComponent } from './components/config/config.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { Error404Component } from './components/error404/error404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ClientesComponent,
+    TableroComponent,
+    EditarClienteComponent,
+    LoginComponent,
+    SignupComponent,
+    ConfigComponent,
+    FooterComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
