002bb7508a574c14627b2a7f86f723d718f6ed07
Sebastián <sebtstian@gmail.com>
2020-07-06 11:49:01 -0300

    mostrando los datos obtenidos en tabla

--- src/app/components/clientes/clientes.component.ts
+++ src/app/components/clientes/clientes.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ApiFirebaseService } from '../../services/api-firebase.service';
+import { Cliente } from 'src/app/interfaces/cliente';
+
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html',
-  styleUrls: ['./clientes.component.css']
+  styleUrls: ['./clientes.component.css'],
 })
 export class ClientesComponent implements OnInit {
+  clientes: Array<Cliente>;
+  loading: boolean;
 
-  constructor() { }
+  constructor(private api: ApiFirebaseService) {}
 
   ngOnInit(): void {
+    this.loading = true;
+    this.api.getClientes().subscribe((resp) => {
+      this.clientes = resp;
+      this.loading = false;
+    });
   }
-
 }
